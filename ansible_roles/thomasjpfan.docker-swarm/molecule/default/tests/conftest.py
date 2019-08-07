5e9a5bd8c9630aacbc9e6176e3ab466f064a853d
Elie Gavoty <eliegavoty@free.fr>
2019-08-08 00:28:27 +0200

    remove roles

--- ansible_roles/thomasjpfan.docker-swarm/molecule/default/tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-
-@pytest.fixture(scope="module")
-def docker_info(host):
-    cmd = "docker info"
-    output = host.check_output(cmd)
-    return output
--- ansible_roles/thomasjpfan.docker-swarm/molecule/default/tests/test_worker_nodes.py
+++
@@ -1,5 +0,0 @@
-testinfra_hosts = ['instance-3', 'instance-4']
-
-
-def test_node_is_worker(docker_info):
-    assert "Is Manager: false" in docker_info
