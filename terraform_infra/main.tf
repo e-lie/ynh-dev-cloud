b05b612a52bf3e65c1cdb7730d1f24e001f667ea
Elie Gavoty <eliegavoty@free.fr>
2019-08-08 00:26:42 +0200

    version start

--- terraform_infra/main.tf
+++ terraform_infra/main.tf
@@ -1,20 +1,20 @@
 variable "do_api_token" {}
 variable "do_sshkey_id" {}
 
-provider "digitalocean" {
-    token = "${var.do_api_token}"
-}
+<provider>
 
+
+# Variables
 locals {
-  swarm_manager_count = 1
-  swarm_worker_count = 4
+  swarm_manager_count = <num> 
+  swarm_worker_count = <num>
 }
 
 resource "digitalocean_droplet" "managers" {
   count = "${local.swarm_manager_count}"
   name = "swarm-manager-${count.index}"
-  image = "ubuntu-18-04-x64"
-  size = "1gb"
+  image = "<image>"
+  size = "2gb"
   region = "ams3"
   ssh_keys = ["${var.do_sshkey_id}"]
 }
@@ -22,31 +22,9 @@ resource "digitalocean_droplet" "managers" {
 resource "digitalocean_droplet" "workers" {
   count = "${local.swarm_worker_count}"
   name = "swarm-worker-${count.index}"
-  image = "ubuntu-18-04-x64"
+  image = "<image>"
   size = "1gb"
   region = "ams3"
   ssh_keys = ["${var.do_sshkey_id}"]
 }
 
-
-## Ansible mirroring hosts section
-# Using https://github.com/nbering/terraform-provider-ansible/ to be installed manually (third party provider)
-# Copy binary to ~/.terraform.d/plugins/
-
-resource "ansible_host" "ansible_managers" {
-  count = "${local.swarm_manager_count}"
-  inventory_hostname = "manager_${count.index}"
-  groups = ["docker_swarm_manager", "swarm_nodes"]
-  vars = {
-    ansible_host = "${element(digitalocean_droplet.managers.*.ipv4_address, count.index)}"
-  }
-}
-
-resource "ansible_host" "ansible_workers" {
-  count = "${local.swarm_worker_count}"
-  inventory_hostname = "worker_${count.index}"
-  groups = ["docker_swarm_worker", "swarm_nodes"]
-  vars = {
-    ansible_host = "${element(digitalocean_droplet.workers.*.ipv4_address, count.index)}"
-  }
-}
